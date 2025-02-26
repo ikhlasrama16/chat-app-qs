a528b735d4e1d667c3b85bf92f5188ba1d6b8d3f
ikhlasrama16 <ikhlasrama16@gmail.com>
2025-02-26 23:35:32 +0700

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data-1.js");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req.params.id);
+
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  5000,
+  console.log(`Server running on port http://localhost:${PORT}`)
+);
